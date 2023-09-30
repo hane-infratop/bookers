a48ed92484ac66663893b5bfc4c631f8e16742ca
hane-infratop <143814316+hane-infratop@users.noreply.github.com>
2023-09-30 15:22:26 +0000

    [Add] createのバリデーション等

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,44 @@
 class BooksController < ApplicationController
+
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'books/index'
+    end
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
